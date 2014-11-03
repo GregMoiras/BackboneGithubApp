22866a68e3b73f92479d474e825a5658f57f05f2
Greg Moiras <gregmoiras@gmail.com>
2014-11-03 17:40:43 +0000

    final refactor collection and view

--- githubdata.js
+++ githubdata.js
@@ -1,111 +1,113 @@
-//returns a callable function
-var template = _.template($('#github-details').html()); 
+var Assignment = {};
 
-var Repository = Backbone.Model.extend({
+Assignment.url = 'https://api.github.com/users/7geese/repos';
 
-});
+// Returns a callable function
+Assignment.template = _.template($('#github-details').html());
 
-var RepositoryCollection = Backbone.Collection.extend({
-  model: Repository,
-  url: 'https://api.github.com/users/7geese/repos',
-  parse: function (res){
-    // loop through each repository's string/name
-    for (var i=0; i < res.length; i++) { 
-      //replace dashes with spaces
-      var replacing = res[i].name.replace(/\-/g, ' '); 
-      //split string(name) into words and place them into an array
-      var repoName = replacing.split(' '); 
-      var foobar = [];
-      //loop through the split words of each repo. capitalize first letter of words except if words are 'on' or 'as'
-      for (var j = 0; j < repoName.length; j++) {
-        if ((repoName[j] == 'on') || (repoName[j] == 'as')) {
-          foobar.push(repoName[j]);
+Assignment.RepoModel = Backbone.Model.extend();
+
+Assignment.RepoCollection = Backbone.Collection.extend({
+    model: Assignment.RepoModel,
+    url: Assignment.url,
+
+    initialize: function() {
+        this.fetch();
+    },
+
+    fetch: function(options) {
+        options = options ? _.clone(options) : { };
+
+        var success = options.success;
+        options.success = function (collection, response, options) {
+            collection.trigger('fetched_and_notified', collection);
+            if (success)
+                success(collection, response, options);
+        };
+        Backbone.Collection.prototype.fetch.call(this, options);
+    },
+
+    replaceCharacters: function(word) {
+        return word.replace(/\w\S*/g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    },
+
+    parse: function (res) {
+        // loop through each repository's string/name
+        for (var i = 0; i < res.length; i++) {
+            //replace dashes with spaces
+            var replacing = res[i].name.replace(/\-/g, ' ');
+
+            //split string(name) into words and place them into an array
+            var repoName = replacing.split(' ');
+
+            var processedWords = [];
+            //loop through the split words of each repo. capitalize first letter of words except if words are 'on' or 'as'
+            for (var j = 0; j < repoName.length; j++) {
+                if ((repoName[j] == 'on') || (repoName[j] == 'as')) {
+                    processedWords.push(repoName[j]);
+                }
+                else {
+                    processedWords.push(this.replaceCharacters(repoName[j]));
+                }
+            }
+            //replace the repo's name with the processed name
+            res[i].name = processedWords.join().replace(/,/g, ' ');
         }
-        else { 
-          foobar.push(
-            repoName[j].replace(/\w\S*/g, function(txt){
-              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-            })
-          );
-        }
-      }
-    //replace the repo's name with the processed name   
-    res[i].name = foobar.join().replace(/,/g, ' '); 
+
+        // Sort processed response by forks_count descending.
+        return _.sortBy(res, 'forks_count').reverse();
     }
-
-    var sorted = _(res).sortBy('forks_count').reverse();
-    console.log('sorted is...', sorted);
-    return sorted; 
-    // this.$el.html(template({ data: sorted }));  
-  }
 });
 
-var GithubDataList = Backbone.View.extend({
-  el: '.wrapper',
-  //renders the view by calling jsonp api.
-  render: function (){ 
-    // console.log(arguments)
-    //variable that is defined, to be able to pass "this" scope to the anonymous function following success.
-    var that = this; 
-    // $.ajax({
-    //   type: 'GET',
-    //   dataType: 'jsonp',
-    //   url: 'https://api.github.com/users/7geese/repos',
-      
-    //   success: function(res){
-    //     // loop through each repository's string/name
-    //     for (var i=0; i < res.data.length; i++) { 
-    //       //replace dashes with spaces
-    //       var replacing = res.data[i].name.replace(/\-/g, ' '); 
-    //       //split string(name) into words and place them into an array
-    //       var repoName = replacing.split(' '); 
-    //       var foobar = [];
-    //       //loop through the split words of each repo. capitalize first letter of words except if words are 'on' or 'as'
-    //       for (var j = 0; j < repoName.length; j++) {
-    //         if ((repoName[j] == 'on') || (repoName[j] == 'as')) {
-    //           foobar.push(repoName[j]);
-    //         }
-    //         else { 
-    //           foobar.push(
-    //             repoName[j].replace(/\w\S*/g, function(txt){
-    //               return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    //             })
-    //           );
-    //         }
-    //       }
-    //     //replace the repo's name with the processed name   
-    //     res.data[i].name = foobar.join().replace(/,/g, ' '); 
-    //     }
-  
-    //     var sorted = _(res.data).sortBy('forks_count').reverse();
-    //     that.$el.html(template({ data: sorted }));  
-    //   }
-    // });
-  var collectionTest = new RepositoryCollection();
+// Define the view
+Assignment.RepoListView = Backbone.View.extend({
+    el: '.wrapper',
 
-    collectionTest.fetch({
-      success: function (collection, responce) {
-        console.log('inside view',collection, responce);
-      }
-    });
-    
+    initialize: function() {
+        if (repoCollection.models.length > 0) {
+            this.initView();
+        }
+        //when view is initialized it listens for changes in repoCollection and calls initView
+        this.listenTo(repoCollection, 'fetched_and_notified', this.initView);
+    },
+    //listens to repoCollection for changes 
+    initView: function() {
+        this.listenTo(repoCollection, 'add remove', this.render);
+        this.render();
+    },
 
-  }
+    //renders the view by calling jsonp api.s
+    render: function () {
+        this.$el.html(Assignment.template({ data: repoCollection }));
+    }
+});
+
+Assignment.Router = Backbone.Router.extend({
+    routes: {
+        '': 'home'
+    }
 });
 
 
-var Router = Backbone.Router.extend({
-  routes: {
-    '': 'home'
-  }
-});
+/**
+ * Defining global variables
+ */
 
-var dataList = new GithubDataList();
+// Instantiate the collection
+var repoCollection = new Assignment.RepoCollection();
 
-var router = new Router();
+// Defining an empty repoListView
+var repoListView;
 
-router.on('route:home', function(){
-  dataList.render();
+var router = new Assignment.Router();
+
+// Setting the default route of the application
+router.on('route:home', function () {
+    if (typeof(repoListView) === "undefined") {
+        repoListView = new Assignment.RepoListView();
+    }
 });
 
 Backbone.history.start();
